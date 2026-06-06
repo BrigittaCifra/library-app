8ddb42a759b590f416f8f5c76070a4af288c6d3d
Brigitta Cifra <brigittacifra@hotmail.com>
2026-06-06 14:09:15 +0200

    feat: add user registration endpoint

---
+++ backend/Routes/Users.cs
@@ -0,0 +1,34 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+//importerar alla klasser från models mappen. Utan 'using' behöver man skriva ut hela sökvägen
+using backend.Models;
+
+namespace backend.Routes;
+
+public static class UserEndpoints
+{
+    public static void MapUserEndpoints(this WebApplication app)
+    {
+
+        // POST skapar en användare
+        // Mappar det inkommande JSON objektet till ett Book objekt genom Book book
+        app.MapPost("/user", async (AppDbContext db, User user) =>
+        {
+            try
+            {
+                db.Users.Add(user);
+                await db.SaveChangesAsync();
+                return Results.Created($"/user/{user.Id}", user); // 201
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        });
+
+        //TBA: 
+        //Inloggning
+        //Auth
+
+    }
+}
