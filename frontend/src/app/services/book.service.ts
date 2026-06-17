0d0e340230426a4e3521fef32391ee1e185afd70
Brigitta Cifra <brigittacifra@hotmail.com>
2026-06-17 12:01:12 +0200

    fix: update frontend enviroment URL

--- frontend/src/app/services/book.service.ts
+++ frontend/src/app/services/book.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { BookModel } from '../models/book.model';
 
 import { AuthStore } from '../stores/auth.store';
--- frontend/src/app/services/quote.service.ts
+++ frontend/src/app/services/quote.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { QuoteModel } from '../models/quote.model';
 
 import { AuthStore } from '../stores/auth.store';
